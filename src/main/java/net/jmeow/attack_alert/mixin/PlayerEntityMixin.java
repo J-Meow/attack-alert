3d7c81394ee2dcd1a378ef45bd142d3b53b32d3b
Jmeow <j@jmeow.net>
2025-07-10 13:58:35 -0500

    Log messages every tick someone within your render distance is holding a sword

---
+++ src/main/java/net/jmeow/attack_alert/mixin/PlayerEntityMixin.java
@@ -0,0 +1,27 @@
+package net.jmeow.attack_alert.mixin;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin {
+    @Shadow public abstract GameProfile getGameProfile();
+
+    @Shadow private ItemStack selectedItem;
+
+    @Inject(at = @At("HEAD"), method = "tick()V")
+    private void init(CallbackInfo info) {
+        if(!this.getGameProfile().equals(MinecraftClient.getInstance().getGameProfile()) && this.selectedItem.getItem().getTranslationKey().endsWith("_sword")) {
+            System.out.println("Player nearby, holding sword");
+        }
+    }
+}

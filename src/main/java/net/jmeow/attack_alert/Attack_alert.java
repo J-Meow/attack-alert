bad5f1f4ad51a5393c1db8375df0387967d8390c
Jmeow <j@jmeow.net>
2025-07-10 12:17:24 -0500

    Set up Fabric mod

---
+++ src/main/java/net/jmeow/attack_alert/Attack_alert.java
@@ -0,0 +1,10 @@
+package net.jmeow.attack_alert;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Attack_alert implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/jmeow/attack_alert/client/Attack_alertClient.java
@@ -0,0 +1,10 @@
+package net.jmeow.attack_alert.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Attack_alertClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
